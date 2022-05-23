98edaad44f3e17725a655f738959310487a901de
digital-sector <specificvector@protonmail.com>
2022-05-23 19:25:02 +1000

    Added dynamic routes and Event Details component.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="event-card">
-    <span>@ {{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-  </div>
+  <router-link :to="{ name: 'EventDetials', params: { id: event.id } }">
+    <div class="event-card">
+      <span>@ {{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
   name: "EventCard",
   props: {
-    event: Object,
+    event: {
+      type: Object,
+      required: true,
+    },
   },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventListView from "../views/EventListView.vue";
 import About from "../views/AboutView.vue";
+import EventDetails from "../views/EventDetails.vue";
 
 const routes = [
   {
@@ -12,6 +13,12 @@ const routes = [
     path: "/about",
     name: "about",
     component: About,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetials",
+    props: true,
+    component: EventDetails,
   },
 ];
 
